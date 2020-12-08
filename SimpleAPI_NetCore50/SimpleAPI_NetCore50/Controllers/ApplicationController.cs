c8622d99c19eacfec2e96687abfc8a2824a871e9
catapart <catapart@outlook.com>
2020-12-08 09:12:49 -0600

    added application controller
    implemented version endpoint;

---
+++ SimpleAPI_NetCore50/SimpleAPI_NetCore50/Controllers/ApplicationController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SimpleAPI_NetCore50.Data;
+using SimpleAPI_NetCore50.Models;
+using System.Reflection;
+
+namespace SimpleAPI_NetCore50.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationController : ControllerBase
+    {
+        private readonly SimpleApiContext DatabaseContext;
+
+        public ApplicationController(SimpleApiContext context)
+        {
+            DatabaseContext = context;
+        }
+
+        // GET: api/Application
+        [HttpGet("version")]
+        public async Task<ActionResult> Version()
+        {
+            string appVersion = Assembly.GetEntryAssembly().GetName().Version.ToString();
+            return await Task.FromResult(Ok(appVersion));
+        }
+
+        // GET: api/Application
+        [HttpGet("bootstrap")]
+        public async Task<ActionResult> Bootstrap()
+        {
+            return Ok();
+        }
+    }
+}
